79e1251695ed31e41848521c0ba5b7c8a52e321b
sarabrizolla <sara13brizolla@gmail.com>
2026-03-20 10:23:07 -0300

    ADICIONEI A PASTA SARA

---
+++ sara/operadores.php
@@ -0,0 +1,15 @@
+<?php
+$contador=0;
+    echo  ++ $contador;
+    echo "<br>";
+
+    echo ++$contador;
+    echo "<br>";
+    
+$contador=0;
+    echo ++$contador;
+    echo "<br>";
+
+    echo $contador ++;
+    echo "<br>";
+?>
